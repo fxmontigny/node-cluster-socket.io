dd6e21a5af53912d0217d237acf25834b855f965
François-xavier Montigny <fxaviermontigny@gmail.com>
2018-10-31 17:04:37 +0100

    [Add] Add ping client / server

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # node-cluster-socket.io
 Test cluster and socket io
+
+# How to start redis
--- index.js
+++ index.js
@@ -8,6 +8,13 @@ var express = require('express'),
 var port = 3000,
   num_processes = require('os').cpus().length
 
+const log = function() {
+  console.log.apply(
+    this,
+    ['PID:', process.pid, '-'].concat(Object.values(arguments))
+  )
+}
+
 if (cluster.isMaster) {
   // This stores our workers. We need to keep them to be able to reference
   // them based on source IP address. It's also useful for auto-restart,
@@ -20,7 +27,7 @@ if (cluster.isMaster) {
 
     // Optional: Restart worker on exit
     workers[i].on('exit', function(code, signal) {
-      console.log('respawning worker', i)
+      log('respawning worker', i)
       spawn(i)
     })
   }
@@ -55,13 +62,20 @@ if (cluster.isMaster) {
     .listen(port)
 } else {
   // Note we don't use a port here because the master listens on it for us.
+  log('Forker stated -> http://localhost:3000')
   var app = new express()
+
+  // Here create public folder to insert html test pages
+  app.use(express.static(__dirname + '/public'))
 
   // Here you might use middleware, attach routes, etc.
 
   // Don't expose our internal server to the outside.
   var server = app.listen(0, 'localhost'),
     io = sio(server)
+
+  // add cors
+  io.origins(['http://localhost:3000'])
 
   // Tell Socket.IO to use the redis adapter. By default, the redis
   // server is assumed to be on localhost:6379. You don't have to
@@ -81,5 +95,19 @@ if (cluster.isMaster) {
     server.emit('connection', connection)
 
     connection.resume()
+  })
+
+  // add socket listener
+  io.on('connection', function(socket) {
+    log('new socket', socket.id)
+
+    socket.on('ping-client', function() {
+      log('receive: ping-client')
+
+      setTimeout(() => {
+        log('sent: ping-server')
+        socket.emit('ping-server')
+      }, 1000)
+    })
   })
 }
